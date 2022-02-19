7e0f7a1b3c6016fc48606f43c59281f7ee37710d
Arden Sinclair <general@ardensinclair.com>
2022-02-19 15:22:36 -0800

    Move source into svelte file router

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,60 @@
-<a href="#" class="container-fluid text-decoration-none">
-	<h1 class="h1 bg-primary text-white">Balancr</h1>
-</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary" style="background-color: #225AEA;">
+	<div class="container-fluid">
+		<!-- <a href="#" class="navbar-brand">s
+					<img src="images/logo.svg" height="28" alt="Brandlogo">
+				</a> -->
+		<div class="navbar-nav">
+			<a href="#" class="container-fluid text-decoration-none ">
+				<h1 class="h1 bg-primary text-white title">Balancr</h1>
+			</a>
+		</div>
+		<button
+			type="button"
+			class="navbar-toggler"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarCollapse"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<div class="navbar-nav ms-auto">
+				<a href="#" class="nav-item nav-link text-white">Sign Out</a>
+			</div>
+		</div>
+	</div>
+</nav>
+
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+	<ul class="nav nav-pills flex-column mb-auto">
+		<li class="nav-item">
+			<a href="#" class="nav-link active" aria-current="page">
+				<svg class="bi me-2" width="16" height="16"><use xlink:href="#home" /></svg>
+				Overview
+			</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link link-dark">
+				<svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2" /></svg>
+				Calendar
+			</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link link-dark">
+				<svg class="bi me-2" width="16" height="16"><use xlink:href="#table" /></svg>
+				Tasks
+			</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link link-dark">
+				<svg class="bi me-2" width="16" height="16"><use xlink:href="#grid" /></svg>
+				Quotes
+			</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link link-dark">
+				<svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle" /></svg>
+				Get Help
+			</a>
+		</li>
+	</ul>
+</div>
