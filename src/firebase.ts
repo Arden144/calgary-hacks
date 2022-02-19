fb9ca7d4e6da9e59ad6dc5ca49e1208ca4229b9b
Arden Sinclair <general@ardensinclair.com>
2022-02-19 11:01:45 -0800

    Initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const config = {
+  apiKey: "AIzaSyBZzS3a6H8jcxQXbAQnMKMXQs0kU0WvdRs",
+  authDomain: "calgaryhacks-aaa07.firebaseapp.com",
+  projectId: "calgaryhacks-aaa07",
+  storageBucket: "calgaryhacks-aaa07.appspot.com",
+  messagingSenderId: "38940125148",
+  appId: "1:38940125148:web:e12befbe60f273a0091119"
+}
+
+export const app = initializeApp(config)
+export const auth = getAuth(app)
+export const store = getFirestore(app)
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<div>
+	<h1>Calgary Hacks 2022</h1>
+</div>
+
+<style lang="scss">
+	div {
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	h1 {
+	}
+</style>
